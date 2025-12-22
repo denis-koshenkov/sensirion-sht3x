0a25dfbcf9e94670a3653a430b9f3e2de5b74baa
Denis Koshenkov <dkoshenkov@gmail.com>
2025-12-22 05:53:32 +0100

    Add driver source files to the build

--- run_test.sh
+++ run_test.sh
@@ -3,4 +3,4 @@ set -e
 
 cmake -GNinja -B build -S . -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 cmake --build build --
-./build/src/test
+./build/src/test/test
---
+++ src/sht3x.h
@@ -0,0 +1,5 @@
+#ifndef SRC_SHT3X_H
+#define SRC_SHT3X_H
+
+
+#endif /* SRC_SHT3X_H */
